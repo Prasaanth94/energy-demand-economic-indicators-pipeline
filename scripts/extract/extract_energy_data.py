0891abe0960a1fac04140b9d76b08b3a262c34c9
M Prasaanth <prasaanth2208@gmail.com>
2026-01-05 17:03:33 +0800

    Updated codes
    Added new lines and .env for api key

--- scripts/extract/extract_energy_data.py
+++ scripts/extract/extract_energy_data.py
@@ -1,10 +1,21 @@
 
 import requests
 import os
+import json
+from dotenv import load_dotenv
 
-def fect_energy_data():
+load_dotenv()
+
+def fetch_energy_data():
+
+    api_key = os.getenv("EIA_API_KEY")
+    if no api_key:
+        print("Error: EIA_API_KEY not set")
+        return
+
     url = "https://api.eia.gov/v2/electricity/retail-sales/data/"
     params = {
+        "api_key" : api_key
         "frequency" : "monthly",
         "data[0]" : "sales",
         "facets[sectorid][]" : "ALL",
@@ -17,11 +28,14 @@ def fect_energy_data():
 
     if response.status_code == 200:
         os.makedirs("data/raw", exist_ok=True)
+        data = response.json()
         with open("data/raw/energy_data.json", "w") as f:
-            f.write(response.text)
+            json.dump(data, f, indent=2)
         print("Energy data saved successfully.")
+        print("Sample record:")
+        print(data["response"]["data"][0])
     else:
-        print("Failed to fetch energy data.")
+        print("Failed to fetch energy data. Status code: {response.status_code}")
 
 
 if __name__ == "__main__":
